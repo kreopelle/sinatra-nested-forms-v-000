fa4e8873d08b3eb2ee27ee49d4186b1ea1037cfa
Kayla Reopelle <kreopelle@gmail.com>
2018-07-13 19:42:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,7 +15,8 @@ module FormsLab
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
 
-      params[:pirate][:ships]
+      params[:pirate][:ships].each do |ship|
+        
 
       erb :'/pirates/show'
     end
